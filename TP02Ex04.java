b53240fcbd8efd9f2f25ac73b50c0a0630bb944a
Alice <133613280+alice-marinho@users.noreply.github.com>
2024-03-29 23:01:16 -0300

    Update TP02Ex04.java

--- TP02Ex04.java
+++ TP02Ex04.java
@@ -6,13 +6,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] matriz ={
-                {"Hoje ", "está ", "nublado"},
-                {"Amanhã ", "estará ", "sol"}
+        int[][] matriz ={
+                {2,3,4},
+                {5,6,7}
         };
 
-        System.out.println(matriz[0][0] + matriz[0][1] + matriz[0][2]);
-        System.out.println(matriz[1][0] + matriz[1][1] + matriz[1][2]);
+        for (int i=0; i < matriz.length; i++){
+            for (int j=0; j< matriz[i].length; j++){
+                System.out.print( matriz[i][j] + " | " );
+            }
+            System.out.println();
+        }
 
     }
 }
