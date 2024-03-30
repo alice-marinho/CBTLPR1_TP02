ad833f5bc161b868465265dc90a9a72981f3e69a
Alice <133613280+alice-marinho@users.noreply.github.com>
2024-03-29 23:01:50 -0300

    Update TP02Ex05.java

--- TP02Ex05.java
+++ TP02Ex05.java
@@ -6,15 +6,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] matriz ={
-                {"Marina ", "Silva"},{"João ", "Marinho"}, {"Karina ", "Gomes"}
+        int[][] matriz ={
+                {2,3},
+                {4,5},
+                {6,7}
         };
 
-        // Marina Silva
-        System.out.println(matriz[0][0] + matriz[0][1]);
-        // João Marinho
-        System.out.println(matriz[1][0] + matriz[1][1]);
-        // Karina Gomes
-        System.out.println(matriz[2][0] + matriz[2][1]);
+        for (int i=0; i < matriz.length; i++){
+            for (int j=0; j< matriz[i].length; j++){
+                System.out.print( matriz[i][j] + " | " );
+            }
+            System.out.println();
+        }
+
     }
 }
