c0a2e9b58feac3e7d87a3b1a45ae4e0de8e715a2
Alice <133613280+alice-marinho@users.noreply.github.com>
2024-03-29 22:09:30 -0300

    Create TP02Ex04.java

---
+++ TP02Ex04.java
@@ -0,0 +1,19 @@
+/*
+Armazenar seis valores em uma matriz de ordem 2x3.
+Apresentar os valores na tela.
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] matriz ={
+                String[][] matriz ={
+                {"Hoje ", "está ", "nublado"},
+                {"Amanhã ", "estará ", "sol"}
+        };
+
+        System.out.println(matriz[0][0] + matriz[0][1] + matriz[0][2]);
+        System.out.println(matriz[1][0] + matriz[1][1] + matriz[1][2]);
+
+    }
+}
